5b1c11962dcc1683299b0f34d642d6edcc778461
hande <hande.dann@gmail.com>
2025-02-10 20:27:57 +0300

    data.json & random answers

--- src/components/Ball.jsx
+++ src/components/Ball.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Ball({ handleClick, isShaking }) {
+  const [list, setList] = useState(null);
+  const [answer, setAnswer] = useState("8");
+
+  useEffect(() => {
+    axios
+      .get("data.json")
+      .then((response) => {
+        setList(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (isShaking && list) {
+      const randomIndex = Math.floor(Math.random() * Object.keys(list).length);
+      const result = list["ans_" + randomIndex];
+      setAnswer(result);
+    }
+  }, [isShaking, list]);
+
   return (
-    <div className="flex justify-center items-center mt-20">
+    <div className="flex justify-center items-center mt-14">
       <div
         className={`${
           isShaking ? "animate-shake" : ""
-        } bg-gradient-to-tl from-black to-stone-700 p-10 border rounded-full w-80 h-80 flex justify-center items-center`}
+        } bg-gradient-to-tl from-black to-stone-700 p-10 border rounded-full w-96 h-96 flex justify-center items-center cursor-pointer`}
         onClick={handleClick}
       >
-        <div className="border bg-gradient-to-tl from-gray-400 to-white p-10 rounded-full w-32 h-32 flex justify-center items-center">
-          <span className="text-4xl font-bold">8</span>
+        <div className="border bg-gradient-to-tl from-gray-400 to-white p-10 rounded-full w-36 h-36 flex justify-center items-center">
+          <div
+            style={{
+              fontSize: answer === "8" || isShaking ? "4em" : "1em",
+              lineHeight: 1.3,
+            }}
+            className="text-4xl font-bold text-center text-indigo-800"
+          >
+            {isShaking ? "8" : answer}
+          </div>
         </div>
       </div>
     </div>
