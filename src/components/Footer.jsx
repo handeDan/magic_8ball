3bd88f8fb74531230811b55d7ccd0eabd00a9a86
hande <hande.dann@gmail.com>
2025-02-10 21:06:40 +0300

    Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-center items-center flex-col mt-9 text-sm">
+      <p>&copy; 2025 Magic 8-Ball. All rights reserved.</p>
+      <p>Made with love for fun and curiosity!</p>
+    </div>
+  );
 }
 
 export default Footer;
