9e4815c31b9bd9e653cbb6b1deb8ddc7037fb579
hande <hande.dann@gmail.com>
2025-02-10 12:31:24 +0300

    Ball component

--- src/components/Ball.jsx
+++ src/components/Ball.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Ball() {
-  return <div>Ball</div>;
+  return (
+    <div className="flex justify-center items-center mt-20">
+      <div className="bg-gradient-to-tl from-black to-stone-700 p-10 border rounded-full w-80 h-80 flex justify-center items-center">
+        <div className="border bg-gradient-to-tl from-gray-400 to-white p-10 rounded-full w-32 h-32 flex justify-center items-center">
+          <span className="text-4xl font-bold">8</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Ball;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="flex justify-between items-center p-4 bg-gray-800 text-white">
+    <div className="flex justify-between items-center p-4 bg-white">
       <p className="text-xl">MAGIC 8BALL</p>
       <a href="">usage</a>
     </div>
