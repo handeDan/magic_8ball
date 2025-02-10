fc2b1c097d2734f0179f832f07927254700ae8c1
hande <hande.dann@gmail.com>
2025-02-10 12:58:32 +0300

    8ball shaking animation

--- src/components/Ball.jsx
+++ src/components/Ball.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Ball() {
+  const [isShaking, setIsShaking] = useState(false);
+  const handleClick = () => {
+    // Animasyonu başlat
+    setIsShaking(true);
+    // Animasyon tamamlandıktan sonra durdur
+    setTimeout(() => setIsShaking(false), 1500); // animasyon süresi 500ms
+  };
+
   return (
     <div className="flex justify-center items-center mt-20">
-      <div className="bg-gradient-to-tl from-black to-stone-700 p-10 border rounded-full w-80 h-80 flex justify-center items-center">
+      <div
+        className={`${
+          isShaking ? "animate-shake" : ""
+        } bg-gradient-to-tl from-black to-stone-700 p-10 border rounded-full w-80 h-80 flex justify-center items-center`}
+        onClick={handleClick}
+      >
         <div className="border bg-gradient-to-tl from-gray-400 to-white p-10 rounded-full w-32 h-32 flex justify-center items-center">
           <span className="text-4xl font-bold">8</span>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // React dosyalarınızı da buraya ekleyin
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shake: "shake 0.2s ease-in-out",
+      },
+      keyframes: {
+        shake: {
+          "0%": { transform: "rotate(30deg)" },
+          "25%": { transform: "rotate(3deg)" },
+          "50%": { transform: "rotate(0deg)" },
+          "75%": { transform: "rotate(-3deg)" },
+          "100%": { transform: "rotate(-30deg)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
